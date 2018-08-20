58f6d92200de554482a2212bd4003faa73979c7b
BobbyRaduloff <bobbyraduloff@gmail.com>
2018-08-20 16:52:02 +0300

    Fixed up usage.php

--- usage.php
+++ usage.php
@@ -1,6 +1,4 @@
 <!DOCTYPE html>
-<!-- Temporary positioning until bootstrap !-->
-<!-- Needs code witch which you doesn't allow you to enter this webpage without permission !-->
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -8,34 +6,19 @@
 		<title> Usage </title>
 		<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="styles/style.css">
-		<link rel="stylesheet" type="text/css" href="styles\usage.css">
+		<link rel="stylesheet" type="text/css" href="styles/usage.css">
 	</head>
 	<body>
 		<?php
 			include('default_header.html');
 			include('connect.php');
-
 			session_start();
-			?>
-		<div class = "interface">
-			<form method="POST">
-				<button class = "btn btn-success" id = "input_button" name = "input">  Input Information </button>
-				<button class = "btn btn-danger" id = "output_button"  name="output"> Output Of Databases </button>
-				<button class = "btn btn-secondary" id = "back_button" name="back"> Back</button>
-			</form>
+			check(2);
+		?>
+		<div>
+			<button class="btn btn-success btn-xlg" name="input" onclick="location.href = 'database_selector_input.php';"> Input </button>
+			<button class="btn btn-danger btn-xlg"  name="output" onclick="location.href = 'database_selector_output.php';"> Output </button>
+			<button class="btn btn-secondary btn-xlg" name="back" onclick="location.href = 'index.php';"> Back </button>
 		</div>
-		<?php
-			if(isset($_POST['input'])) {
-			  header("Location:database_selector_input.php");
-			}
-
-			if(isset($_POST['output'])) {
-			  header("Location:database_selector_output.php");
-			}
-
-			if(isset($_POST['back'])) {
-			  header("Location:index.php");
-			}
-			?>
 	</body>
 </html>
